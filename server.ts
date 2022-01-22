5a992692437986cfef308135fcf2bb9fc60a9953
kimrine <347642700@qq.com>
2022-01-21 19:01:08 -0500

    add

--- server.ts
+++ server.ts
@@ -2,16 +2,11 @@ import express from 'express';
 const app = express();
 
 app.get('/hello', (req, res) =>
-  res.send('Hello World!'));
+    res.send('Hello World!'));
+
+app.get('/add/:a/:b', (req, res) => {
+    res.send(req.params.a + req.params.b);
+})
 
 const PORT = 4000;
-app.listen(PORT);
-// load the express library
-// create an instance of the library
-
-// use express library to listen for URL pattern "/hello"
-// respond with string "Hello World!"
-
-
-// listen to port 4000
-
+app.listen(process.env.PORT || PORT);
